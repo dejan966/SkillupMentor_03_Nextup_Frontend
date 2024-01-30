870a3b924107cdfa86317dcbb1d3ef430d62a264
dejan966 <59951914+dejan966@users.noreply.github.com>
2024-01-30 20:54:54 +0100

    Set primary and secondary color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Provider from '@/components/providers/Provider'
 import { Poppins } from 'next/font/google'
+import './globals.css'
 
 const poppins = Poppins({ subsets: ['latin'], weight: '300' })
 
