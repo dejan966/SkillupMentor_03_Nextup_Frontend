3e044b3dcf715310abf009d84e8a0f5f3e9f8131
dejan966 <59951914+dejan966@users.noreply.github.com>
2024-02-01 11:34:55 +0100

    Added pictures and split footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,8 +1,12 @@
+'use client'
+import useDeviceSize from '@/hooks/useDeviceSize'
 import Image from 'next/image'
 
 export default function Footer() {
+  const [width, height] = useDeviceSize()
+  console.log(width)
   return (
-    <footer className="bg-white">
+    <footer className="bg-white" style={{ width: width / 2 }}>
       <div className="flex justify-between items-center pl-24 pb-9 pr-24 pt-9">
         <Image
           src="/nextup-logo.png"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="pl-24">
-      <div className="text-black font-bold">FIND THE BEST EVENTS</div>
+      <div className="grid grid-cols-3">
+        <div>
+          <div className="text-black font-bold">FIND THE BEST EVENTS</div>
+          <h1 className="text-6xl font-bold">
+            Are you looking for your next event?
+          </h1>
+          <br />
+          <div>sfshfdhjk</div>
+          <br />
+          <div className="text-black font-bold">FIND YOUR NEXT EVENT</div>
+          <div>dfd</div>
+        </div>
+        <div>
+          <Image
+            src="/picture-1.png"
+            alt="Picture 1"
+            width={300}
+            height={200}
+          />
+        </div>
+        <div>
+          <Image
+            src="/picture-2.png"
+            alt="Picture 2"
+            height={200}
+            width={200}
+          />
+        </div>
+      </div>
     </main>
   )
 }
