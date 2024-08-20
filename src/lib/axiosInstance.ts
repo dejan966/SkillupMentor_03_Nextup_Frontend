f2034853c4c79dde88ac145be328d5981f4fba67
dejan966 <ultimate24208@gmail.com>
2024-08-20 19:50:46 +0200

    Create axios instance

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = null;
+
+    if (token) {
+      if (config.headers) config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
